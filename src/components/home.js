4d2b4f958ca36294cefffec04f400a77c8b11e8f
dnlbell <dnl.bell@gmail.com>
2019-07-11 13:26:58 -0700

    updating resume

--- src/components/home.js
+++ src/components/home.js
@@ -28,7 +28,7 @@ function Home () {
         <h2> Welcome! </h2>
         <p>Thank you for visiting my portfolio. Your time is valuable and your willingness to come here is appreciated. On this site you will find references to projects I have been directly involved in or wholly my own creation. This site itself was put together by me using React, Material-UI, and other JavaScript libraries. There is also a section containing my resume, with downloadable versions available. </p>
         <p>Over the last two years I have pursued and completed a Bachelor of Applied Science degree in application development. This program exposed me to web, mobile, and cloud-based tech stacks. You will find references to some of the code I have created from these classes in the projects section of this site. Many of these projects were collaborative, using Git to manage source control, and agile style project management to set priorities and timetables.</p>
-        <p>In congruence with my education, I have been employed as a Helpdesk Specialist by the architecture firm MG2. I have been with this company for nearly five years providing technical support from their flagship office in Seattle.</p>
+        <p>In conjunction with my education, I have been employed as a Helpdesk Specialist by the architecture firm MG2. I have been with this company for nearly five years providing technical support from their flagship office in Seattle.</p>
         <p>Looking toward my future, I seek to find new challenges to help grow my skills as a software developer. I hope that what you see on this site will encourage you to consider my skills for such challenges you may be seeking a champion for.</p>
         <p>Thanks for visiting!</p>
         <p><i><strong>~ Daniel Bell</strong></i></p>
